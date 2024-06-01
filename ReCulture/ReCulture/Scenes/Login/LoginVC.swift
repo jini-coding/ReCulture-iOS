669cb3935cba8cd1c558f39122a51bfa6fb6bca5
Suyeon <syhhwang1231@gmail.com>
2024-06-02 02:19:59 +0900

    Feat: 앱 첫 실행 시 로그인 화면 구현

--- ReCulture/ReCulture/Scenes/Login/LoginVC.swift
+++ ReCulture/ReCulture/Scenes/Login/LoginVC.swift
@@ -9,30 +9,211 @@ import UIKit
 
 class LoginVC: UIViewController {
 
-    let titleLabel : UILabel = {
+    // MARK: - Views
+    
+    private let logoImageView: UIImageView = {
+        let view = UIImageView()
+        view.backgroundColor = .rcGrayBg
+        return view
+    }()
+    
+    private let welcomeLabel: UILabel = {
         let label = UILabel()
-        label.text = "LoginVC"
-        
+        label.text = "Re:Culture"
+        label.font = .rcFont26B()
+        return label
+    }()
+    
+    private let emailTextField: CustomTextField = {
+        let tf = CustomTextField()
+        tf.placeholder = "이메일을 입력해주세요"
+        return tf
+    }()
+    
+    private let passwordTextField: CustomTextField = {
+        let tf = CustomTextField()
+        tf.placeholder = "비밀번호를 입력해주세요"
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    
+    private let loginButton: CustomButton = {
+        let button = CustomButton(title: "로그인")
+        button.addTarget(self, action: #selector(loginButtonDidTap), for: .touchUpInside)
+        return button
+    }()
+    
+    private let signUpButton: CustomButton = {
+        let button = CustomButton(title: "회원가입")
+        button.addTarget(self, action: #selector(signUpButtonDidTap), for: .touchUpInside)
+        return button
+    }()
+    
+    private let noAccountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "계정이 없다면?"
+        label.font = .rcFont14M()
+        label.textColor = .rcGray400
         return label
     }()
 
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor.white
         
-        setupTitleLabel()
+        addKeyboardObserver()
+        
+        setupLogoImageView()
+        setupWelcomeLabel()
+        setEmailTextField()
+        setPasswordTextField()
+        setupLoginButton()
+        setNoAccountLabel()
+        setupSignUpButton()
     }
     
-    func setupTitleLabel() {
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+    override func viewWillAppear(_ animated: Bool) {
+        removeKeyBoardObserver()
+    }
+    
+    // MARK: - Layout
+    
+    private func setupLogoImageView(){
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
         
-        view.addSubview(titleLabel)
+        view.addSubview(logoImageView)
         
         NSLayoutConstraint.activate([
-            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            logoImageView.widthAnchor.constraint(equalToConstant: 100),
+            logoImageView.heightAnchor.constraint(equalTo: logoImageView.widthAnchor),
+            logoImageView.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor),
+            logoImageView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 60),
+            
         ])
+    }
+    
+    private func setupWelcomeLabel(){
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(welcomeLabel)
+        
+        NSLayoutConstraint.activate([
+            welcomeLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            welcomeLabel.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 30)
+        ])
+    }
+    
+    private func setEmailTextField(){
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(emailTextField)
+        
+        NSLayoutConstraint.activate([
+            emailTextField.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 40),
+            emailTextField.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -40),
+            emailTextField.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 45),
+            emailTextField.heightAnchor.constraint(equalToConstant: 42)
+        ])
+    }
+    
+    private func setPasswordTextField(){
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(passwordTextField)
+        
+        NSLayoutConstraint.activate([
+            passwordTextField.leadingAnchor.constraint(equalTo: emailTextField.leadingAnchor),
+            passwordTextField.trailingAnchor.constraint(equalTo: emailTextField.trailingAnchor),
+            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 10),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 42)
+        ])
+    }
+    
+    private func setupLoginButton(){
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(loginButton)
+        
+        NSLayoutConstraint.activate([
+            loginButton.leadingAnchor.constraint(equalTo: passwordTextField.leadingAnchor),
+            loginButton.trailingAnchor.constraint(equalTo: passwordTextField.trailingAnchor),
+            loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 10),
+            loginButton.heightAnchor.constraint(equalToConstant: 42)
+        ])
+    }
+    
+    private func setNoAccountLabel(){
+        noAccountLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(noAccountLabel)
+        
+        NSLayoutConstraint.activate([
+            noAccountLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            noAccountLabel.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 40),
+        ])
+    }
+    
+    private func setupSignUpButton(){
+        signUpButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(signUpButton)
+        
+        NSLayoutConstraint.activate([
+            signUpButton.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 40),
+            signUpButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -40),
+            signUpButton.topAnchor.constraint(equalTo: noAccountLabel.bottomAnchor, constant: 15),
+            signUpButton.heightAnchor.constraint(equalToConstant: 42)
+        ])
+    }
+    
+    // MARK: - Actions
+    
+    @objc private func loginButtonDidTap(){
+        print("로그인 버튼 선택됨")
+    }
+    
+    @objc private func signUpButtonDidTap(){
+        print("회원가입하러 가기")
+    }
+    
+    @objc private func keyboardWillShow(_ notification: NSNotification){
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height / 2
+            }
+        }
+    }
+    
+    @objc private func keyboardWillHide(_ notification: NSNotification){
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+    // MARK: - Functions
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    private func addKeyboardObserver() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow(_:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide(_:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    private func removeKeyBoardObserver() {
+        NotificationCenter.default.removeObserver(UIResponder.keyboardWillShowNotification)
+        NotificationCenter.default.removeObserver(UIResponder.keyboardWillHideNotification)
     }
 }
 
