5d25c801c23792250e91233bf74af57803e38bad
Suyeon <syhhwang1231@gmail.com>
2024-06-07 13:51:04 +0900

    Fix: KeyChain 접근 쿼리 오류 수정

--- ReCulture/ReCulture/Global/KeychainManager.swift
+++ ReCulture/ReCulture/Global/KeychainManager.swift
@@ -6,21 +6,26 @@
 //
 
 import Foundation
+import Security
+
+enum TokenType: String{
+    case accessToken = "AccessToken"
+    case refreshToken = "RefreshToken"
+}
 
 class KeychainManager {
     
     static let shared = KeychainManager()
     
-    private let service = "com.smwu.ReCulture"
-    private let account = "token"
-    
+    static let service = Bundle.main.bundleIdentifier
+        
     /// KeyChain에 토큰 저장
-    func saveToken(_ token: String) {
-        let query: NSDictionary = [
-           kSecClass: kSecClassGenericPassword,
-           kSecAttrService: service,
-           kSecAttrAccount: account,
-           kSecValueData: token.data(using: .utf8, allowLossyConversion: false) as Any   // 저장할 Token
+    func saveToken(type: TokenType, token: String) {
+        let query: [String : Any] = [
+           kSecClass as String: kSecClassGenericPassword as Any,
+           kSecAttrService as String: KeychainManager.service as Any,
+           kSecAttrAccount as String: type.rawValue as Any,
+           kSecValueData as String: token.data(using: .utf8, allowLossyConversion: false) as Any   // 저장할 Token
         ]
         
         // 기존에 아이템이 있는 경우 먼저 삭제해야 함
@@ -28,7 +33,7 @@ class KeychainManager {
         
         // 새 데이터 추가
         let status = SecItemAdd(query as CFDictionary, nil)
-        
+        print("토큰 저장: \(status)")
         // 데이터 추가 결과가 no error가 아닌 경우
         guard status == errSecSuccess else {
             print("키체인에 토큰 추가 - 에러 발생: \(status)")
@@ -37,18 +42,18 @@ class KeychainManager {
     }
     
     /// KeyChain에서 토큰 읽어오기
-    func getToken() -> String? {
-        let query: NSDictionary = [
-            kSecClass: kSecClassGenericPassword,
-            kSecAttrService: service,
-            kSecAttrAccount: account,
-            kSecReturnData: kCFBooleanTrue as Any,  // CFData 타입으로 불러오라는 의미
-            kSecMatchLimit: kSecMatchLimitOne       // 중복되는 경우, 하나의 값만 불러오라는 의미
+    func getToken(type: TokenType) -> String? {
+        let query: [String : Any] = [
+            kSecClass as String: kSecClassGenericPassword as Any,
+            kSecAttrService as String : KeychainManager.service as Any,
+            kSecAttrAccount as String: type.rawValue as Any,
+            kSecReturnData as String: kCFBooleanTrue as Any,  // CFData 타입으로 불러오라는 의미
+            kSecMatchLimit as String: kSecMatchLimitOne as Any       // 중복되는 경우, 하나의 값만 불러오라는 의미
         ]
         
         // 값 읽어오기
         var dataTypeRef: AnyObject?
-        let status = SecItemCopyMatching(query, &dataTypeRef)
+        let status = SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
         
         if status == errSecSuccess {
             let retrievedData = dataTypeRef as! Data
@@ -62,14 +67,14 @@ class KeychainManager {
     }
     
     /// KeyChain에서 토큰 삭제
-    func deleteToken() -> Bool {
-        let query: NSDictionary = [
-            kSecClass: kSecClassGenericPassword,
-            kSecAttrService: service,
-            kSecAttrAccount: account
+    func deleteToken(type: TokenType) -> Bool {
+        let query: [String : Any] = [
+            kSecClass as String: kSecClassGenericPassword as Any,
+            kSecAttrService as String: KeychainManager.service as Any,
+            kSecAttrAccount as String: type.rawValue as Any
         ]
         
-        let status = SecItemDelete(query)
+        let status = SecItemDelete(query as CFDictionary)
         
         if status == errSecSuccess {
             print("토큰이 키체인에서 삭제됨")
