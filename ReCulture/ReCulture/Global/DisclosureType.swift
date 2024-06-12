2658201bd6256227d79368c151f1f54977a96b68
Suyeon <syhhwang1231@gmail.com>
2024-06-12 21:48:33 +0900

    Feat: 공개여부 type global로 추가

---
+++ ReCulture/ReCulture/Global/DisclosureType.swift
@@ -0,0 +1,13 @@
+//
+//  DisclosureType.swift
+//  ReCulture
+//
+//  Created by Suyeon Hwang on 6/12/24.
+//
+
+import Foundation
+
+enum DisclosureType: String {
+    case Public = "PUBLIC"
+    case Private = "PRIVATE"
+}
