9b2d875e558ae23a770568394534166ca6ff0388
Suyeon <syhhwang1231@gmail.com>
2024-06-12 22:04:06 +0900

    Feat: 기록 추가 응답 dto 및 api 통신 코드 추가

---
+++ ReCulture/ReCulture/Networking/APIs/AddRecordAPI.swift
@@ -0,0 +1,29 @@
+//
+//  AddRecordAPI.swift
+//  ReCulture
+//
+//  Created by Suyeon Hwang on 6/12/24.
+//
+
+import Foundation
+
+struct AddRecordAPI: ServableAPI {
+    typealias Response = AddRecordResponseDTO
+    
+    let requestDTO: [String: Any]
+    let photos: [ImageFile]
+    let boundary = UUID().uuidString
+    
+    var method: HTTPMethod { .post }
+    var path: String { "/culture" }
+    var headers: [String : String]? { [
+        "Authorization": "Bearer \(KeychainManager.shared.getToken(type: .accessToken)!)",
+        "Content-Type": "multipart/form-data; boundary=\(boundary)"
+    ]}
+    
+    var multipartRequestBody: Data? {
+        createBody(parameters: requestDTO,
+                   boundary: boundary,
+                   imageFiles: photos)
+    }
+}
