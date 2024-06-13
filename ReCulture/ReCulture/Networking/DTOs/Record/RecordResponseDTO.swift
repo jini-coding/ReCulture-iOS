7c1b6e3fffd2a68863b627b4e4bc2cb281292968
Suyeon <syhhwang1231@gmail.com>
2024-06-14 00:59:49 +0900

    Fix: Record 조회 응답 unableToDecode 해결

--- ReCulture/ReCulture/Networking/DTOs/Record/RecordResponseDTO.swift
+++ ReCulture/ReCulture/Networking/DTOs/Record/RecordResponseDTO.swift
@@ -5,49 +5,70 @@
 //  Created by Jini on 6/3/24.
 //
 
+
 struct RecordResponseDTO: Codable {
-    struct Culture: Codable {
-        let id: Int
-        let title: String
-        let emoji: String
-        let date: String
-        let categoryId: Int
-        let authorId: Int
-        let disclosure: String
-        let review: String
-        let detail1: String
-        let detail2: String
-        let detail3: String
-        let detail4: String
-    }
+    let id: Int
+    let title: String
+    let emoji: String
+    let date: String
+    let categoryId: Int
+    let disclosure: String
+    let review: String
+    let detail1: String
+    let detail2: String
+    let detail3: String
+    let detail4: String
+    let authorId: Int
+    let createdAt: String
+    let updatedAt: String
+    
+    let photos: [PhotoDoc]
+    
+//    struct Culture: Codable {
+//        let id: Int
+//        let title: String
+//        let emoji: String
+//        let date: String
+//        let categoryId: Int
+//        let disclosure: String
+//        let review: String
+//        let detail1: String
+//        let detail2: String
+//        let detail3: String
+//        let detail4: String
+//        let authorId: Int
+//        let createdAt: String
+//        let updatedAt: String
+//    }
     
     struct PhotoDoc: Codable {
-        let culturePostId: Int
+        let id: Int
         let url: String
+        let culturePostId: Int
     }
     
-    let culture: Culture
-    let photoDocs: [PhotoDoc]
+//    let culture: Culture
+//    let photos: [PhotoDoc]
 }
 
 extension RecordResponseDTO {
     static func convertRecordDTOToModel(DTO: RecordResponseDTO) -> RecordModel {
         return RecordModel(
             culture: RecordModel.Culture(
-                id: DTO.culture.id,
-                title: DTO.culture.title,
-                emoji: DTO.culture.emoji,
-                date: DTO.culture.date,
-                categoryId: DTO.culture.categoryId,
-                authorId: DTO.culture.authorId,
-                disclosure: DTO.culture.disclosure,
-                review: DTO.culture.review,
-                detail1: DTO.culture.detail1,
-                detail2: DTO.culture.detail2,
-                detail3: DTO.culture.detail3,
-                detail4: DTO.culture.detail4
+                id: DTO.id,
+                title: DTO.title,
+                emoji: DTO.emoji,
+                date: DTO.date,
+                categoryId: DTO.categoryId,
+                authorId: DTO.authorId,
+                disclosure: DTO.disclosure,
+                review: DTO.review,
+                detail1: DTO.detail1,
+                detail2: DTO.detail2,
+                detail3: DTO.detail3,
+                detail4: DTO.detail4
             ),
-            photoDocs: DTO.photoDocs.map { photoDoc in
+            photoDocs: DTO.photos.map { photoDoc in
                 RecordModel.PhotoDoc(
                     culturePostId: photoDoc.culturePostId,
                     url: photoDoc.url
