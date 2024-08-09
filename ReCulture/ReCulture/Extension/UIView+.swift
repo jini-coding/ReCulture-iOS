5e7f894ea3d0da767435113afbe1a51d7bada9ed
Suyeon <syhhwang1231@gmail.com>
2024-08-09 18:57:39 +0900

    Feat: UIView를 이미지로 변환하는 UIView Extension 추가

--- ReCulture/ReCulture/Extension/UIView+.swift
+++ ReCulture/ReCulture/Extension/UIView+.swift
@@ -44,4 +44,18 @@ extension UIView {
 
         self.layer.addSublayer(borderLayer)
     }
+    
+    /// UIView를 이미지로 변환하는 함수
+    /// - Returns: 뷰 내용을 담은 이미지
+    func transfromToImage() -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(bounds.size, isOpaque, 0.0)
+        defer {
+            UIGraphicsEndImageContext()
+        }
+        if let context = UIGraphicsGetCurrentContext() {
+            layer.render(in: context)
+            return UIGraphicsGetImageFromCurrentImageContext()
+        }
+        return nil
+    }
 }
